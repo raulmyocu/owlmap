098407291803b36125c9c6bc92bb9977313ba52f
raulmyocu <rauloctaviomurcia@gmail.com>
2019-03-19 11:05:27 -0700

    SE AGREGA MAESTROS

--- owlmapdb.sql
+++ owlmapdb.sql
@@ -16,13 +16,39 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `lugares`
+-- Table structure for table `maestros`
 --
 
-DROP TABLE IF EXISTS `lugares`;
+DROP TABLE IF EXISTS `maestros`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `lugares` (
+CREATE TABLE `maestros` (
+  `expediente` varchar(11) NOT NULL,
+  `apellidos` varchar(63) NOT NULL,
+  `nombres` varchar(63) NOT NULL,
+  `correo` varchar(255) NOT NULL,
+  `cubiculo` varchar(7) DEFAULT NULL,
+  PRIMARY KEY (`expediente`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `maestros`
+--
+
+LOCK TABLES `maestros` WRITE;
+/*!40000 ALTER TABLE `maestros` DISABLE KEYS */;
+/*!40000 ALTER TABLE `maestros` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `puntos`
+--
+
+DROP TABLE IF EXISTS `puntos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `puntos` (
   `latitud` double NOT NULL,
   `longitud` double NOT NULL,
   `descripcion` varchar(511) DEFAULT NULL,
@@ -33,13 +59,13 @@ CREATE TABLE `lugares` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `lugares`
+-- Dumping data for table `puntos`
 --
 
-LOCK TABLES `lugares` WRITE;
-/*!40000 ALTER TABLE `lugares` DISABLE KEYS */;
-INSERT INTO `lugares` VALUES (29.0828752,-110.9617472,'Comedor Universitario','12d-1','servicio'),(29.0828752,-110.9617472,'Tesoreria','12d-2','servicio'),(29.0812894,-110.9601972,'Edificio de Ciencias de la Computacion','3k-4','edificio');
-/*!40000 ALTER TABLE `lugares` ENABLE KEYS */;
+LOCK TABLES `puntos` WRITE;
+/*!40000 ALTER TABLE `puntos` DISABLE KEYS */;
+INSERT INTO `puntos` VALUES (29.0828752,-110.9617472,'Comedor Universitario','12d-1','servicio'),(29.0828752,-110.9617472,'Tesoreria','12d-2','servicio'),(29.0812894,-110.9601972,'Edificio de Ciencias de la Computacion','3k-4','edificio');
+/*!40000 ALTER TABLE `puntos` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -50,12 +76,12 @@ DROP TABLE IF EXISTS `usuarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usuarios` (
-  `expediente` int(11) NOT NULL,
-  `apellidos` varchar(63) DEFAULT NULL,
-  `nombres` varchar(63) DEFAULT NULL,
+  `expediente` varchar(11) NOT NULL,
+  `apellidos` varchar(63) NOT NULL,
+  `nombres` varchar(63) NOT NULL,
   `correo` varchar(127) NOT NULL,
-  `pass` varchar(255) DEFAULT NULL,
-  `powers` enum('normal','mod','admin') DEFAULT NULL,
+  `pass` varchar(255) NOT NULL,
+  `powers` enum('normal','mod','admin') DEFAULT 'normal',
   PRIMARY KEY (`expediente`,`correo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -78,4 +104,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-15 18:22:33
+-- Dump completed on 2019-03-19 18:04:47
