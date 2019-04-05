dd93665e34d206b696aa987635371a0ccabe9714
Ximena <ximena.sandoval.dh@gmail.com>
2019-04-05 16:48:17 -0700

    Regex para la entrada del correo, eliminación de validator email

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, valid
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class LoginForm(FlaskForm):
-    email = StringField('Correo institucional', validators=[DataRequired(), Email()],
+    email = StringField('Correo institucional', validators=[DataRequired()],
             render_kw={"placeholder": "Correo institucional"})
     password = PasswordField('Contrasena', validators=[DataRequired()],
             render_kw={"placeholder": "Contrasena"})
