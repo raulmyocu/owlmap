6112c131667e3091839f671fac815c653984637d
raulmyocu <rauloctaviomurcia@gmail.com>
2019-04-06 19:08:23 -0700

    se agrega el icono de posicion

--- owlmap/static/js/map.js
+++ owlmap/static/js/map.js
@@ -5,9 +5,17 @@ L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
     subdomains:['mt0','mt1','mt2','mt3']
 }).addTo(map);
 
+var buhoIcono = L.icon({
+    iconUrl: 'static/images/owlmapPuntero.png',
+
+    iconSize:     [46, 53], // size of the icon
+    iconAnchor:   [23, 52], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
 navigator.geolocation.getCurrentPosition(function(position) {
   const coords = [position.coords.latitude, position.coords.longitude];
-  const locMarker = L.marker(coords);
+  const locMarker = L.marker(coords, {icon: buhoIcono});
   locMarker.bindPopup('Aquí estás').openPopup();
   map.addLayer(locMarker);
 });
